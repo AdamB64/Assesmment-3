8fb29c15245e06e59dcce3aa02e29b6c3ecb4c74
Karma63 <adam2004buchan@outlook.com>
2023-01-11 20:33:57 +0000

    new

--- Assessment 3.py
+++ Assessment 3.py
@@ -39,95 +39,94 @@ print(f.toString())
 p=True
 r = None
 count = 1
-#while s > 0:
-p=True
-if count == 1:
-    r = p1
-    print("Player 1")
-elif count ==2:
-    r = p2
-    print("Player 2")
-    count =0
+while g.caught ==False:
+    if count == 1:
+        r = p1
+        print("Player 1")
+    elif count ==2:
+        r = p2
+        print("Player 2")
+        count =0
 #getting command from the end user
-print("you are currently here \n" + r.toString())
-command = input("\nWhich direction do you want to move D=down,U+up,L=left and R=right or press Q to quit ")
-command = command.upper()
-while command not in ("D","U","L","R","Q"):
-    command = input("You did not type D U L R or Q, try again ")
+    print("you are currently here \n" + r.toString())
+    command = input("\nWhich direction do you want to move D=down,U+up,L=left and R=right or press Q to quit ")
     command = command.upper()
-if command == "Q":
-    quit()
+    while command not in ("D","U","L","R","Q"):
+        command = input("You did not type D U L R or Q, try again ")
+        command = command.upper()
+    if command == "Q":
+        quit()
 #a if statement to check if the user is going down and that he is able to
-if f.canMove(x+1,y) == True and command =="D":
+    if f.canMove(x+1,y) == True and command =="D":
     # a if statement to check if the direction the user is going in has a sprout 
     #if f.getCharAtPos(x+1,y) =="@":
         #f.eatSprouts()
         #r.sproutAte()
         #s = s -1
     #function to clear the last postion 
-    f.clearAtPos(x,y)
+        f.clearAtPos(x,y)
     #function to move the x axis down 
-    x= r.moveDown()
+        x= r.moveDown()
     #function to place the rat on the maze
-    f.placePlayer(play[count-1].getChar,x,y)
+        f.placePlayer(play[count-1].getChar,x,y)
 # a elif that says if not able to move in that direction tell user
-elif f.canMove(x+1,y) == False and command =="D":
-    print("\nCant go that way pick another direction")
-    p=False
+    elif f.canMove(x+1,y) == False and command =="D":
+        print("\nCant go that way pick another direction")
+        p=False
 #a if statement to check if the user is going up  and that he is able to
-if f.canMove(x-1,y) == True and command == "U":
+    if f.canMove(x-1,y) == True and command == "U":
     # a if statement to check if the direction the user is going in has a sprout 
     #if f.getCharAtPos(x-1,y)=="@":
         #f.eatSprouts()
         #r.sproutAte()
         #s = s -1
     #function to clear the last postion
-    f.clearAtPos(x,y)
+        f.clearAtPos(x,y)
     #function to move the x axis up
-    x =r.moveUp()
+        x =r.moveUp()
     #function to place the rat on the maze
-    f.placePlayer(play[count-1].getChar,x,y)
+        f.placePlayer(play[count-1].getChar,x,y)
 # a elif that says if not able to move in that direction tell user
-elif f.canMove(x-1,y) == False and command == "U":
-    print("\nCant go that way, pick another direction")
-    p=False
+    elif f.canMove(x-1,y) == False and command == "U":
+        print("\nCant go that way, pick another direction")
+        p=False
 #a if statement to check if the user is going left and that he is able to
-if f.canMove(x,y-1) == True and command =="L":
+    if f.canMove(x,y-1) == True and command =="L":
     # a if statement to check if the direction the user is going in has a sprout 
     #if f.getCharAtPos(x,y-1)=="@":
         #f.eatSprouts()
        # r.sproutAte()
        # s = s -1
     #function to clear the last postion
-    f.clearAtPos(x,y)
+        f.clearAtPos(x,y)
     #function to move the x axis left
-    y = r.moveLeft()
+        y = r.moveLeft()
     #function to place the rat on the maze
-    f.placePlayer(play[count-1].getChar,x,y)
+        f.placePlayer(play[count-1].getChar,x,y)
 # a elif that says if not able to move in that direction tell user
-elif f.canMove(x,y-1) ==False and command =="L":
-    print("\nCant go that way, pick another direction")
-    p=False
+    elif f.canMove(x,y-1) ==False and command =="L":
+        print("\nCant go that way, pick another direction")
+        p=False
 #a if statement to check if the user is going right and that he is able to
-if f.canMove(x,y+1) == True and command == "R":
+    if f.canMove(x,y+1) == True and command == "R":
     # a if statement to check if the direction the user is going in has a sprout 
     #if f.getCharAtPos(x,y+1) =="@":
         #f.eatSprouts()
        # r.sproutAte()
       #  s = s -1
     #function to clear the last postion
-    f.clearAtPos(x,y)
+        f.clearAtPos(x,y)
     #function to move the x axis right
-    y = r.moveRight()
+        y = r.moveRight()
     #function to place the rat on the maze
-    f.placePlayer(play[count-1].getChar,x,y)
+        f.placePlayer(play[count-1].getChar,x,y)
 # a elif that says if not able to move in that direction tell user
-elif f.canMove(x,y+1) == False and command =="R":
-    print("\nCant go that way, pick another direction")
-    p= False
+    elif f.canMove(x,y+1) == False and command =="R":
+        print("\nCant go that way, pick another direction")
+        p= False
 #a if statement to check how many sprouts the rat has ate
-if p==True:
-    count = count + 1
+    if p==True:
+        count = count + 1
 
 
 
